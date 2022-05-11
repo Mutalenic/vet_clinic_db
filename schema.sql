927d37b5343ac158552e043d59a46a95e8ae2953
Nicholas Mutale <nicomutale@gmail.com>
2022-05-11 15:21:25 +0200

    alter animals table add column

--- schema.sql
+++ schema.sql
@@ -8,3 +8,8 @@ CREATE TABLE animals (
     neutered boolean not null,
     weight_kg real not null
 );
+
+/*Add a column 'species' of type 'string' to the animals table*/
+
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(100);
