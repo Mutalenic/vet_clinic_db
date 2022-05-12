d6480152a0a97c98153896bb68988083fd43cd15
Nicholas Mutale <nicomutale@gmail.com>
2022-05-12 19:37:07 +0200

    List all Digimon owned by Jennifer Orwell

--- queries.sql
+++ queries.sql
@@ -125,3 +125,10 @@ FROM species
 JOIN animals 
 ON species.id = animals.species_id 
 GROUP BY species.name;
+
+--List all Digimon owned by Jennifer Orwell.
+SELECT animals.name 
+FROM species 
+JOIN animals 
+ON species.id = animals.species_id JOIN owners ON owners.id = animals.owner_id   
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
