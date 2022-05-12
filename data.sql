3dbb93f3cdd84775d7c3e5e4a8b20bdae4a2d78a
Nicholas Mutale <nicomutale@gmail.com>
2022-05-12 16:51:46 +0200

    Insert data into the owners table

--- data.sql
+++ data.sql
@@ -20,4 +20,12 @@ VALUES
 ('Blossom', '1998-10-13', 3, true, 17),
 ('Dito', '2022-05-14', 4, true, 22);
 
-
+-- Insert data into the owners table
+INSERT INTO owners(full_name, age)
+VALUES 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
