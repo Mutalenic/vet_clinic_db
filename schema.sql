44b996c37810b1d2fb17b3ea7c4d0b56a1a77fe4
Nicholas Mutale <nicomutale@gmail.com>
2022-05-12 16:41:43 +0200

    Add column owner id which is a foreign key referencing the owners table

--- schema.sql
+++ schema.sql
@@ -36,3 +36,10 @@ ALTER TABLE animals
     ADD CONSTRAINT fk_species
     FOREIGN KEY (species_id)
     REFERENCES species(id);
+
+--Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals 
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
