a5144dee654f236f3a4acfa05c84278f46d1354a
Nicholas Mutale <nicomutale@gmail.com>
2022-05-13 12:35:47 +0200

    Insert data into vets table

--- data.sql
+++ data.sql
@@ -70,3 +70,11 @@ WHERE name IN ('Charmander','Squirtle','Blossom');
 UPDATE animals
 set owner_id = (SELECT id from owners where full_name = 'Dean Winchester')
 WHERE name IN ('Angemon','Boarmon');
+
+-- Insert data into vets table
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
